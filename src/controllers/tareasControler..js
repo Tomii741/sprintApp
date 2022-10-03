11df76bf013b35b748e52b2f434ec89b40d922f4
Juan  Hinojosa <juanma.hinojosa97@gmail.com>
2022-10-03 18:20:22 -0300

    Creacion de V2

--- src/controllers/tareasControler..js
+++ src/controllers/tareasControler..js
@@ -1,3 +1,7 @@
 import "dotenv/config";
 import mongodb from "mongodb";
 const MongoClient = mongodb.MongoClient;
+
+export const home = (req, res) => {
+    res.render("index")
+}
--- src/routes/tareasRouter.js
+++ src/routes/tareasRouter.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import {
-    homeTareas,
-    dameTareas,
-    tareasId,
-    agregarTareas,
+    home,
+    // dameTareas,
+    // tareasId,
+    // agregarTareas,
     // editarTarea,
-    editar,
-    eliminarTarea
+    // editar,
+    // eliminarTarea
 }  from "../controllers/tareasControler.js"
 
 export const router = Router();
+
+router.get("/", home);
+
+// router.get("/tareas", dameTareas);
+// router.get("/tareas/:id", tareasId);
+// router.post("/addTareas", agregarTareas);
+// router.get("/editar", editar);
+// router.put("/editar/:id", editarTarea);
+// router.delete("/eliminar/:id", eliminarTarea);
